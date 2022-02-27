d9f10831e8160d8470e536f94f2442743e877258
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2022-02-27 15:22:45 +0300

    Code refactor

--- CRUDBackend/CRUDBackend/settings.py
+++ CRUDBackend/CRUDBackend/settings.py
@@ -22,16 +22,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y%i6pz!+y#(lgg_*qh3_=)pap9hr37ke7xzyy85s=zs7n=n=2$'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
 
 # Application definition
 
